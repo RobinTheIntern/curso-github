bd71ed6621b2b78f64250200757ef9ecd2cf6eec
Gabriel Dias <54082345+RobinTheIntern@users.noreply.github.com>
2019-09-02 18:19:46 -0300

    Tentativa Remoto
    tentativa de Add file por git

---
+++ Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _4earth_GabrielD_1703749
+{
+    public class Login
+    {
+        private int length;
+
+        public int ID { get { return ID; } set { ID = value; } }
+        public string User_Name { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get => senha; set => senha = value; }
+
+        public Login() { }
+
+        //Tentativa Gerar Hash -- Abstract(N/A) Mother Class
+         public string Encripto(String senhamain)
+        {
+               byte[] bytesSenhamain;
+               byte[] codificar;
+               MD5 mD5;
+
+               mD5 = new MD5CryptoServiceProvider();
+               bytesSenhamain = ASCIIEncoding.Default.GetBytes(senhamain);
+               codificar = mD5.ComputeHash(bytesSenhamain);
+               return BitConverter.ToString(codificar);
+        }
+
+
+        public void EfetuarCadastro()
+
+        {
+
+        }
+    }
+}
