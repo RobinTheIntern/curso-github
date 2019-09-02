96c306f90c454d1e1076fb52a0ed1769c297f91b
Gabriel Dias <54082345+RobinTheIntern@users.noreply.github.com>
2019-09-02 18:17:07 -0300

    Add files via upload

---
+++ Pontuacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4earth_GabrielD_1703749
+{
+    class Pontuacao : Login
+    {
+        public int posicao { get; set; }
+        public int Pontuacao { get; set; }
+        public void Gerarplacar()
+        {
+
+        }
+    }
+}
